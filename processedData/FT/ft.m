8cc0f82309c7177b03eec8d2f2a20d815d5536dd
Akshiv Bansal <akshivbansal@dhcp-128-189-79-33.ubcsecure.wireless.ubc.ca>
2018-03-25 13:38:43 -0700

    added 45 FFT code

--- processedData/FT/ft.m
+++ processedData/FT/ft.m
@@ -2,4 +2,8 @@ clear;
 image = rgb2gray(imread('vert.jpeg'));
 fort  = abs(fft(image));
 mesh(fort);
-view([0,0]);
+%view([0,0]);
+figure(2)
+image45 = rgb2gray(imread('45.jpeg'));
+fort45  = abs(fft(image45));
+mesh(fort45);
