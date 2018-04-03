a3aae7883d92e29b6ecfb872fd9c6dbd0822df69
Akshiv Bansal <akshivbansal@dhcp-128-189-76-108.ubcsecure.wireless.ubc.ca>
2018-04-03 12:38:32 -0700

    Diffraction Code update

--- processedData/Diffraction/diffractionProcessor.m
+++ processedData/Diffraction/diffractionProcessor.m
@@ -1,4 +1,4 @@
-clear; 
+clear; warning('off','all');
 
 imageFiles = [  '20.5.jpeg            ';     %1
                 '20.76.jpeg           '; ... %2
@@ -19,17 +19,75 @@ imageFiles = [  '20.5.jpeg            ';     %1
                 '23.31.jpeg           '];    %16
 
 option = 'hor';
-line   = 275;
+line   = 270:1:280;
 arraySize = 640;
 [fileNum, colm] = size(imageFiles);
 
-imageData = zeros(fileNum, arraySize);
+imageData   = zeros(fileNum, arraySize);
+fourierData = zeros(fileNum, arraySize);
 
-for k = 1 : fileNum
-    
-    file = strtrim(imageFiles(k,:));
-    [x,y] = DiffractionData(file, line, option);
-    
-    imageData(k,:) = y;
+for j = 1 : length(line)
+    for k = 1 : fileNum
+
+        file = strtrim(imageFiles(k,:));
+        [~,y] = DiffractionData(file, line(j), option);
+
+        imageData(k,:) = imageData(k,:) + y; 
+    end
 end
+
+d     = (-arraySize*2:1:arraySize*2) * 7.5e-6;
+dplot = (-arraySize/2 + 1:1:arraySize/2) * 7.5e-6;
+
+close all;
+
+
+
+slitWidth = [20.5, 20.76, 20, 21.04, 21.5, 21.25, 21.86, 21.86, 22.17, ...
+            22.17, 22.57, 22.90, 23.09, 23.09, 23.21, 23.28, 23.31];
+slitWidth = -(slitWidth  - 23.4) / 1000;
+
+R       = 0.5; %3.973;%m
+lambda  = 632.8e-9; %m
+delV = slitWidth * sqrt( 2 / (R*lambda));
+
+
+z    = zeros(length(slitWidth), length(d));
+beta = zeros(length(slitWidth), length(d));
+
+Cs = zeros(length(slitWidth), length(d)-1);
+Ss = zeros(length(slitWidth), length(d)-1);
+I  = zeros(length(slitWidth), length(d)-1);
+
+diffractionIntensity = zeros(length(slitWidth), length(d));
+
+for k = 1:length(slitWidth)
+    z(k,:) = d / slitWidth(k);
+    
+    beta(k,:) = z(k,:) * pi / 2 * (delV(k)) ^ 2;
+    for j = 1:length(d)
+        v1 = - (z(k,j) + 0.5) * delV(k);
+        v2 = - (z(k,j) - 0.5) * delV(k);
+        Cs(k,j) = integral(@(x)cos(pi * x.^2 / 2), v1, v2);
+        Ss(k,j) = integral(@(x)sin(pi * x.^2 / 2), v1, v2);
+        I(k,j) = Cs(k,j)^2 + Ss(k,j)^2;
+    end
+    
+    fourierData(k,:) = fftshift( fft( imageData(k,:)));
+    imageData(k,:) = imageData(k,:) - imageData(k, round(arraySize/30));
+    A = max(imageData(k,:));
+    scalerA = A / max(I(k,:)) ;
+    scalerB = A / max(fourierData(k,:));
+    
+    figure(k)
+    %plot(d, I(k,:)*scalerA);
+    hold on
+    plot(dplot, imageData(k,:));
+    plot(dplot, fourierData(k,:)*scalerB);
+    
+    ylim( [-100,(A+50)]);
+    hold off
+    %diffractionIntensity(i,:) = (delV(i)) ^ 2 * (sin(beta(i,:)) / beta(i,:)) ^ 2;
+end
+
 
