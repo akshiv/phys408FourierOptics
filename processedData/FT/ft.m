745366ebb973d7ba5c22da488b81f51083ef79bc
Akshiv Bansal <akshivbansal@dhcp-128-189-72-138.ubcsecure.wireless.ubc.ca>
2018-04-06 10:35:46 -0700

    fft nozon

--- processedData/FT/ft.m
+++ processedData/FT/ft.m
@@ -1,9 +1,9 @@
 clear;
-image = rgb2gray(imread('vert.jpeg'));
+image = rgb2gray(imread('snap-unknown-20180313-114925-1.jpeg'));
 fort  = abs(fft(image));
 mesh(fort);
-%view([0,0]);
-figure(2)
-image45 = rgb2gray(imread('45.jpeg'));
-fort45  = abs(fft(image45));
-mesh(fort45);
+view([0,0]);
+% figure(2)
+% image45 = rgb2gray(imread('45.jpeg'));
+% fort45  = abs(fft(image45));
+% mesh(fort45);
